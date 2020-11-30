252be35a1b73969a1a16834ac5b62c6b5f6f24e5
Yask Srivastava <yask.srivastava@shopify.com>
2020-11-30 11:41:02 -0500

    add profile picture

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,17 @@ export default function Index() {
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
       <div className="container">
+        <div className="bio"> 
+        <div className="profile-pic">
+          <img src="https://ca.slack-edge.com/E017D49VC3F-W018GAHP2A1-e6d2d7ad1680-512" alt="" className="image--cover" />
+        </div>
         <div>
           <h1>
             Hi, I'm Yask<span className="fancy">.</span>
           </h1>
           <h2>Software Engineer @Shopify.</h2>
           <SocialList />
+        </div>
         </div>
       </div>
       <style jsx>{`
@@ -26,6 +31,10 @@ export default function Index() {
           justify-content: center;
           flex: 1 1 auto;
           padding: 0 1.5rem;
+        }
+        .bio { 
+          display: flex;
+          align-items: center;
         }
         h1 {
           font-size: 2.5rem;
@@ -46,7 +55,19 @@ export default function Index() {
           color: #9b9b9b;
           letter-spacing: 0.05em;
         }
-
+        .image--cover {
+          width: 175px;
+          height: 175px;
+          border-radius: 50%;
+        
+          object-fit: cover;
+          object-position: center left;
+        }
+        .profile-pic { 
+          position: relative;
+          right: 30px;
+          bottom: 10px;
+        }
         @media (min-width: 769px) {
           h1 {
             font-size: 3rem;
